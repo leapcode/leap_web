2c2a80812818362d0e0c416deefd4aee2787dd9e
Azul <azul@leap.se>
2012-10-24 20:50:40 +0200

    removing duplicate testing of helper_methods
    * once tested as helper
    * once tested as @controller.send...

--- users/test/functional/application_controller_test.rb
+++ users/test/functional/application_controller_test.rb
@@ -13,23 +13,10 @@ class ApplicationControllerTest < ActionController::TestCase
     assert_access_denied
   end
 
-  def test_current_user_with_caching
-    @user = stub_logged_in
-    assert_equal @user, @controller.send(:current_user)
-    assert_equal @user, @controller.send(:current_user) # tests caching
-  end
-
   def test_authorized
     @user = stub_logged_in
     @controller.send(:authorize)
     assert_access_denied(false)
-  end
-
-  def test_admin
-    bool = stub
-    @user = stub_logged_in
-    @user.expects(:is_admin?).returns(bool)
-    assert_equal bool, @controller.send(:admin?)
   end
 
   def test_authorize_admin
