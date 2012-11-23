46c0140a8eab632c783d309a7afd87cb7aad4280
Azul <azul@leap.se>
2012-11-23 10:55:49 +0100

    refactored creation of record stubs

--- users/test/functional/users_controller_test.rb
+++ users/test/functional/users_controller_test.rb
@@ -1,6 +1,8 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
+  include StubRecordHelper
+
   test "should get new" do
     get :new
     assert_equal User, assigns(:user).class
@@ -8,11 +10,9 @@ class UsersControllerTest < ActionController::TestCase
   end
 
   test "should create new user" do
-    params = User.valid_attributes_hash
-    user = stub params.merge(:id => 123)
-    params.stringify_keys!
-    User.expects(:create!).with(params).returns(user)
-    post :create, :user => params
+    user = stub_record User
+    User.expects(:create!).with(user.params).returns(user)
+    post :create, :user => user.params
     assert_nil session[:user_id]
     assert_response :redirect
     assert_redirected_to root_url
@@ -31,34 +31,19 @@ class UsersControllerTest < ActionController::TestCase
   end
 
   test "should get edit view" do
-    params = User.valid_attributes_hash
-    user = stub params.merge(:id => 123, :class => User, :to_key => ['123'], :new_record? => false, :persisted? => :true)
+    user = stub_record User
     login user
     get :edit, :id => user.id
     assert_equal user, assigns[:user]
   end
 
   test "should process updated params" do
-    params = User.valid_attributes_hash
-    user = stub params.merge(:id => 123)
-    params.stringify_keys!
-    user.expects(:update).with(params).returns(user)
+    user = stub_record User
+    user.expects(:update).with(user.params).returns(user)
     login user
-    post :update, :user => params, :id => user.id
+    post :update, :user => user.params, :id => user.id
     assert_equal user, assigns[:user]
     assert_response :redirect
     assert_redirected_to edit_user_path(user)
   end
-
-  test "should validate updated params" do
-    params = User.valid_attributes_hash
-    user = stub params.merge(:id => 123)
-    params.stringify_keys!
-    user.expects(:update).with(params).returns(user)
-    login user
-    post :update, :user => params, :id => user.id
-    assert_equal user, assigns[:user]
-  end
-
-
 end
