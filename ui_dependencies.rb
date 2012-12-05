ea3718c34cccd294865c0dd533db0b4aa18015cc
Azul <azul@leap.se>
2012-12-05 18:04:00 +0100

    trying to work around a gem issue with the ruby racer 0.11.n

--- ui_dependencies.rb
+++ ui_dependencies.rb
@@ -11,10 +11,8 @@ group :assets do
   gem "coffee-rails", "~> 3.2.2"
   gem "uglifier", "~> 1.2.7"
 
-  # seems to be required for therubyracer:
-  gem 'libv8', '~> 3.11.8'
   # See https://github.com/sstephenson/execjs#readme for more supported runtimes
-  gem 'therubyracer', :platforms => :ruby
+  gem 'therubyracer', "~> 0.10.2", :platforms => :ruby
 
 end
 
