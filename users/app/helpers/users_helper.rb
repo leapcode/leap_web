af101adb7c66201e175642ff0ef99988b42d2df2
Azul <azul@leap.se>
2012-12-07 10:00:21 +0100

    refactored views to ease adding of email form

--- users/app/helpers/users_helper.rb
+++ users/app/helpers/users_helper.rb
@@ -1,2 +1,22 @@
 module UsersHelper
+
+  def user_form_with(partial, legend, locals)
+    user_form do |f|
+      locals.reverse_merge! :legend => legend, :f => f
+      render :partial => partial,
+        :layout => 'legend_and_submit',
+        :locals => locals
+    end
+  end
+
+  def user_form
+    html_class = 'form-horizontal user form '
+    html_class += (@user.new_record? ? 'new' : 'edit')
+    simple_form_for @user,
+      :validate => true,
+      :format => :json,
+      :html => {:class => html_class} do |f|
+      yield f
+    end
+  end
 end
