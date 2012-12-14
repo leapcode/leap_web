1eeec0808886e305d5113a20bf6ea5c1921e633b
Azul <azul@leap.se>
2012-12-14 14:33:04 +0100

    fixed most of the unit tests
    no idea why the numericality validatoin with greater_than_or_equal 0 does not catch negative numbers

--- certs/test/unit/cert_test.rb
+++ certs/test/unit/cert_test.rb
@@ -3,47 +3,27 @@ require 'test_helper'
 class CertTest < ActiveSupport::TestCase
 
   setup do
-    @sample = Cert.new
-    @sample.set_random
-    @sample.attach_zip
+    @sample = LeapCA::Cert.new LeapCA::Cert.valid_attributes_hash
   end
 
-  test "certs come with attachments" do
-    assert @sample.has_attachment? "cert.txt"
-  end
-
-  test "cert.zip_attachment returns couchDB attachment" do
-    assert_equal "text/plain", @sample.zip_attachment["content_type"]
-  end
-
-  test "cert.zipped returns the actual data" do
-    @sample.save # This is required !
-    assert lines = @sample.zipped.split("\n")
-    assert_equal 56, lines.count
-    assert_equal "-----BEGIN RSA PRIVATE KEY-----", lines.first.chomp
-    assert_equal "-----END CERTIFICATE-----", lines.last.chomp
-  end
-
-  test "cert.zipname returns name for the zip file" do
-    assert_equal "cert.txt", @sample.zipname
-  end
-
-  test "test data is valid" do
+  test "stub cert for testing is valid" do
     assert @sample.valid?
   end
 
   test "validates random" do
-    @sample.stubs(:set_random)
-    [0, 1, nil, "asdf"].each do |invalid|
+    [-1, 1, nil, "asdf"].each do |invalid|
       @sample.random = invalid
       assert !@sample.valid?, "#{invalid} should not be a valid value for random"
     end
   end
 
-  test "validates attachment" do
-    @sample.stubs(:attach_zip)
-    @sample.delete_attachment(@sample.zipname)
-    assert !@sample.valid?, "Cert should require zipped attachment"
+  test "validates key" do
+    @sample.key = nil
+    assert !@sample.valid?, "Cert should require key"
   end
 
+  test "validates cert" do
+    @sample.cert = nil
+    assert !@sample.valid?, "Cert should require cert"
+  end
 end
