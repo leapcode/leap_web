48258a0df7d4715a2b5533d94ef0f5906f08ea96
Azul <azul@leap.se>
2013-01-07 20:06:57 +0100

    prevent requiring leap_ca database settings

--- certs/lib/leap_web_certs/engine.rb
+++ certs/lib/leap_web_certs/engine.rb
@@ -1,5 +1,7 @@
 require "leap_web_core"
-require "leap_ca"
+require "leap_ca/config"
+LeapCA::Config.db_name = "client_certificates"
+require "leap_ca/cert"
 
 module LeapWebCerts
   class Engine < ::Rails::Engine
