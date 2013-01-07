da5718fe3bcc416dc12ec6892dd8a79ce37525d4
Azul <azul@leap.se>
2013-01-07 21:03:51 +0100

    attempting to work around server failure

--- certs/lib/leap_web_certs/engine.rb
+++ certs/lib/leap_web_certs/engine.rb
@@ -1,6 +1,13 @@
 require "leap_web_core"
 require "leap_ca/config"
 LeapCA::Config.db_name = "client_certificates"
+
+# couchrest model has an initializer for this - but apparently that does not work
+CouchRest::Model::Base.configure do |conf|
+  conf.environment = Rails.env
+  conf.connection_config_file = File.join(Rails.root, 'config', 'couchdb.yml')
+end
+
 require "leap_ca/cert"
 
 module LeapWebCerts
