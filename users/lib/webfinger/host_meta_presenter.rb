efb9f511f426b9d6f3af63608e77f80c4b823fb0
Azul <azul@leap.se>
2013-01-23 15:52:38 +0100

    added a small test for HostMetaPresenter and using links hash in xml view

--- users/lib/webfinger/host_meta_presenter.rb
+++ users/lib/webfinger/host_meta_presenter.rb
@@ -5,22 +5,26 @@ class Webfinger::HostMetaPresenter
     @request = request
   end
 
+  def to_json(options = {})
+    {
+      subject: subject,
+      links: links
+    }.to_json(options)
+  end
+
   def subject
     url = URI.parse(@request.url)
     url.path = ''
     url.to_s
   end
 
-  def webfinger_template(path = 'webfinger', query_param='q')
-    "#{subject}/#{path}?#{query_param}={uri}"
+  def links
+    { lrdd: { type: 'application/xrd+xml', template: webfinger_template } }
   end
 
-  def to_json(options = {})
-    {
-      subject: subject,
-      links: {
-        lrdd: { type: 'application/xrd+xml', template: webfinger_template }
-      }
-    }.to_json(options)
+  protected
+
+  def webfinger_template(path = 'webfinger', query_param='q')
+    "#{subject}/#{path}?#{query_param}={uri}"
   end
 end
