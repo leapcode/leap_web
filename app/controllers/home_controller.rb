bb58733e065c7ff5036e75e805f1d498d0085e80
Azul <azul@leap.se>
2013-02-09 14:36:53 +0100

    configure couchrest session store in initializer

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 
 class HomeController < ApplicationController
   def index
+    debugger if params[:debug]
   end
 end
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,10 @@
 # Be sure to restart your server when you modify this file.
 
 LeapWeb::Application.config.session_store CouchRestSessionStore
+
+CouchRestSessionStore.configure do |conf|
+  conf.environment = Rails.env
+  conf.connection_config_file = File.join(Rails.root, 'config', 'couchdb.yml')
+  conf.connection[:prefix] =
+    Rails.application.class.to_s.underscore.gsub(/\/.*/, '')
+end
