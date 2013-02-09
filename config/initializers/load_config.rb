f885f2312b3e3fa8f5e5c926699769416309d1f2
Azul <azul@leap.se>
2013-02-09 13:05:09 +0100

    fall back to {} if config file is empty for environment

--- config/initializers/load_config.rb
+++ config/initializers/load_config.rb
@@ -2,6 +2,6 @@ def load_config_file(file)
   File.exists?(file) ? YAML.load_file(file)[Rails.env] : {}
 end
 
-defaults = load_config_file("#{Rails.root}/config/defaults.yml")
-config = load_config_file("#{Rails.root}/config/config.yml")
+defaults = load_config_file("#{Rails.root}/config/defaults.yml") || {}
+config = load_config_file("#{Rails.root}/config/config.yml") || {}
 APP_CONFIG = defaults.merge(config).with_indifferent_access
