a314d1265bcf7b0c6dd66d61d03e1d2a7545cfb8
Azul <azul@leap.se>
2013-02-25 12:35:00 +0100

    enable free certs in the controller

--- certs/app/controllers/certs_controller.rb
+++ certs/app/controllers/certs_controller.rb
@@ -1,10 +1,8 @@
 class CertsController < ApplicationController
 
-  before_filter :authorize
-
   # GET /cert
   def show
-    @cert = ClientCertificate.new
+    @cert = ClientCertificate.new(free: !logged_in?)
     render :text => @cert.key + @cert.cert, :content_type => 'text/plain'
   end
 
--- certs/test/functional/certs_controller_test.rb
+++ certs/test/functional/certs_controller_test.rb
@@ -4,16 +4,18 @@ class CertsControllerTest < ActionController::TestCase
   setup do
   end
 
-  test "should require login" do
+  test "should send free cert without login" do
+    cert = stub :cert => "free cert", :key => "key"
+    ClientCertificate.expects(:new).with(free: true).returns(cert)
     get :show
-    assert_response :redirect
-    assert_redirected_to login_url
+    assert_response :success
+    assert_equal cert.key + cert.cert, @response.body
   end
 
   test "should send cert" do
     login
     cert = stub :cert => "adsf", :key => "key"
-    ClientCertificate.expects(:new).returns(cert)
+    ClientCertificate.expects(:new).with(free: false).returns(cert)
     get :show
     assert_response :success
     assert_equal cert.key + cert.cert, @response.body
