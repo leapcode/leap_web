9bf6d9e8cc5b42c4fdbf9b37468a12d1476d45ec
Azul <azul@leap.se>
2013-02-26 17:34:36 +0100

    activate strong params in core

--- core/lib/extensions/couchrest.rb
+++ core/lib/extensions/couchrest.rb
@@ -1,26 +1,45 @@
 module CouchRest
-  module Model::Designs
+  module Model
+    module Designs
 
-    class View
+      class View
 
-      # so we can called Ticket.method.descending or Ticket.method.ascending
-      def ascending
-        self
+        # so we can called Ticket.method.descending or Ticket.method.ascending
+        def ascending
+          self
+        end
       end
-    end
 
-    class DesignMapper
-      def load_views(dir)
-        Dir.glob("#{dir}/*.js") do |js|
-          name = File.basename(js, '.js')
-          file = File.open(js, 'r')
-          view name.to_sym,
-            :map => file.read,
-            :reduce => "function(key, values, rereduce) { return sum(values); }"
+      class DesignMapper
+        def load_views(dir)
+          Dir.glob("#{dir}/*.js") do |js|
+            name = File.basename(js, '.js')
+            file = File.open(js, 'r')
+            view name.to_sym,
+              :map => file.read,
+              :reduce => "function(key, values, rereduce) { return sum(values); }"
+          end
         end
       end
     end
 
+    module PropertyProtection
+
+      def remove_protected_attributes(attributes)
+        sanitize_for_mass_assignment(attributes)
+      end
+
+    end
+
+    class Base
+      include ActiveModel::MassAssignmentSecurity
+      include ActiveModel::ForbiddenAttributesProtection
+    end
+
+    module Embeddable
+      include ActiveModel::MassAssignmentSecurity
+      include ActiveModel::ForbiddenAttributesProtection
+    end
   end
 
   class ModelRailtie
