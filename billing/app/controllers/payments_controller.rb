95ec46beac867e1b7df17ba8a72367bbe0b140a9
jessib <jessib@leap.se>
2013-03-28 11:23:14 -0700

    Let users select amount for payments.

--- billing/app/controllers/payments_controller.rb
+++ billing/app/controllers/payments_controller.rb
@@ -1,19 +1,18 @@
 class PaymentsController < ApplicationController
   def new
-    @amount = calculate_amount
     if current_user
       if @customer = Customer.find_by_user_id(current_user.id)
         @braintree_data = Braintree::Customer.find(@customer.braintree_customer_id)
         @default_cc = @braintree_data.credit_cards.find { |cc| cc.default? }
         @tr_data = Braintree::TransparentRedirect.transaction_data(:redirect_url => confirm_payment_url,
-                                                                   :transaction => {:type => "sale", :amount => @amount, :customer_id => @customer.braintree_customer_id, :options => {:submit_for_settlement => true } })
+                                                                   :transaction => {:type => "sale", :customer_id => @customer.braintree_customer_id, :options => {:submit_for_settlement => true } })
       else
         redirect_to new_customer_path
       end
     else
       # anonymous payment not attributed to any user (ie, donation)
       @tr_data = Braintree::TransparentRedirect.transaction_data(:redirect_url => confirm_payment_url,
-                                                                 :transaction => {:type => "sale", :amount => @amount, :options => {:submit_for_settlement => true } })
+                                                                 :transaction => {:type => "sale", :options => {:submit_for_settlement => true } })
     end
 
   end
@@ -23,15 +22,8 @@ class PaymentsController < ApplicationController
     if @result.success?
       render :action => "confirm"
     else
-      @amount = calculate_amount
       render :action => "new"
     end
   end
 
-  protected
-
-  def calculate_amount
-    # in a real app this be calculated from a shopping cart, determined by the product, etc.
-    "100.00"
-  end
 end
