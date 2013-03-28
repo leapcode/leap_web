c436fce774573d090ab77617dbf55b25b2da4ea2
jessib <jessib@leap.se>
2013-03-28 11:49:32 -0700

    Some refactoring

--- billing/app/controllers/payments_controller.rb
+++ billing/app/controllers/payments_controller.rb
@@ -4,15 +4,13 @@ class PaymentsController < ApplicationController
       if @customer = Customer.find_by_user_id(current_user.id)
         @braintree_data = Braintree::Customer.find(@customer.braintree_customer_id)
         @default_cc = @braintree_data.credit_cards.find { |cc| cc.default? }
-        @tr_data = Braintree::TransparentRedirect.transaction_data(:redirect_url => confirm_payment_url,
-                                                                   :transaction => {:type => "sale", :customer_id => @customer.braintree_customer_id, :options => {:submit_for_settlement => true } })
+        @tr_data = transparent_redirect(@customer.braintree_customer_id)
       else
         redirect_to new_customer_path
       end
     else
       # anonymous payment not attributed to any user (ie, donation)
-      @tr_data = Braintree::TransparentRedirect.transaction_data(:redirect_url => confirm_payment_url,
-                                                                 :transaction => {:type => "sale", :options => {:submit_for_settlement => true } })
+      @tr_data = transparent_redirect
     end
 
   end
@@ -25,5 +23,14 @@ class PaymentsController < ApplicationController
       render :action => "new"
     end
   end
+
+  protected
+
+  def transparent_redirect(braintree_customer_id = nil)
+    Braintree::TransparentRedirect.transaction_data(:redirect_url => confirm_payment_url,
+                                                    :transaction => {:type => "sale", :customer_id => braintree_customer_id, :options => {:submit_for_settlement => true } })
+
+  end
+
 
 end
---
+++ billing/app/helpers/braintree_form_helper.rb
@@ -0,0 +1,64 @@
+module BraintreeFormHelper
+  class BraintreeFormBuilder < ActionView::Helpers::FormBuilder
+    include ActionView::Helpers::AssetTagHelper
+    include ActionView::Helpers::TagHelper
+
+    def initialize(object_name, object, template, options, proc)
+      super
+      @braintree_params = @options[:params]
+      @braintree_errors = @options[:errors]
+      @braintree_existing = @options[:existing]
+    end
+
+    def fields_for(record_name, *args, &block)
+      options = args.extract_options!
+      options[:builder] = BraintreeFormBuilder
+      options[:params] = @braintree_params && @braintree_params[record_name]
+      options[:errors] = @braintree_errors && @braintree_errors.for(record_name)
+      new_args = args + [options]
+      super record_name, *new_args, &block
+    end
+
+    def text_field(method, options = {})
+      has_errors = @braintree_errors && @braintree_errors.on(method).any?
+      field = super(method, options.merge(:value => determine_value(method)))
+      result = content_tag("div", field, :class => has_errors ? "fieldWithErrors" : "")
+      result.safe_concat validation_errors(method)
+      result
+    end
+
+    protected
+
+    def determine_value(method)
+      if @braintree_params
+        @braintree_params[method]
+      elsif @braintree_existing
+
+        if @braintree_existing.kind_of?(Braintree::CreditCard)
+
+          case method
+          when :number
+            method = :masked_number
+          when :cvv
+            return nil
+          end
+        end
+
+        @braintree_existing.send(method)
+      else
+        nil
+      end
+    end
+
+    def validation_errors(method)
+      if @braintree_errors && @braintree_errors.on(method).any?
+        @braintree_errors.on(method).map do |error|
+          content_tag("div", ERB::Util.h(error.message), {:style => "color: red;"})
+        end.join
+      else
+        ""
+      end
+    end
+  end
+end
+
