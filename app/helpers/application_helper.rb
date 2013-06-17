8b7621793fe5f8fa55c955045a71821c53d4759f
elijah <elijah@riseup.net>
2013-06-17 01:26:35 -0700

    start of new ui - css changes, layout changes, navigation changes.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
+
+  #
+  # markup for bootstrap icon
+  #
+  # http://twitter.github.io/bootstrap/base-css.html#icons
+  #
+  def icon(name, color=nil)
+    if color.nil?
+      color_class = nil
+    elsif color == :black
+      color_class = 'icon-black'
+    elsif color == :white
+      color_class = 'icon-white'
+    end
+    "<i class=\"icon-#{name} #{color_class}\"></i> ".html_safe
+  end
+
 end
