52846835676ecbf57fe2e1f3613cc50e2c486ade
elijah <elijah@riseup.net>
2013-06-19 00:34:11 -0700

    remove gem bootstrap-editable

--- ui_dependencies.rb
+++ ui_dependencies.rb
@@ -4,9 +4,16 @@ gem "jquery-rails"
 gem "simple_form"
 gem 'client_side_validations'
 gem 'client_side_validations-simple_form'
-gem 'kaminari', "0.13.0" # for pagination. trying 0.13.0 as there seem to be issues with 0.14.0 when using couchrest 
-gem 'bootstrap-editable-rails', "~>0.0.4"
 
+gem 'kaminari', "0.13.0" # for pagination. trying 0.13.0 as there seem to be
+                         # issues with 0.14.0 when using couchrest
+
+# gem 'bootstrap-editable-rails', "~>0.0.4"
+
+gem 'rails-i18n'  # locale files for built-in validation messages and times
+                  # https://github.com/svenfuchs/rails-i18n
+                  # for a list of keys:
+                  # https://github.com/svenfuchs/rails-i18n/blob/master/rails/locale/en.yml
 group :assets do
   gem "haml-rails", "~> 0.3.4"
   gem "sass-rails", "~> 3.2.5"
