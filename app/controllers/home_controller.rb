93999b107fff7275970012c3f12c6877d2b16d6e
elijah <elijah@riseup.net>
2013-06-19 00:36:16 -0700

    redirect to overview if authenticated

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,9 @@
 
 class HomeController < ApplicationController
   def index
+    if logged_in?
+      redirect_to user_overview_url(current_user)
+    end
     debugger if params[:debug]
   end
 end
