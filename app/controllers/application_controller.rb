4863ce5e78d880f1ca8a1874cd03022afb061f4a
elijah <elijah@riseup.net>
2013-06-28 20:38:14 -0700

    new layout for the home buttons

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,16 @@ class ApplicationController < ActionController::Base
 
   ActiveSupport.run_load_hooks(:application_controller, self)
 
+  protected
+
+  #
+  # Allows us to pass through bold text to flash messages. See format_flash() for where this is reversed.
+  #
+  # TODO: move to core
+  #
+  def bold(str)
+    "[b]#{str}[/b]"
+  end
+  helper_method :bold
+
 end
