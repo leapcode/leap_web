0cd386e0144601f5478f90bbdb401d55c019c828
elijah <elijah@riseup.net>
2013-07-03 11:11:10 -0700

    better ticket view navigation: tickets are now either global in scope (for admins) or stay as a nested resource for a particular user (for normal users and when you visit the tickets list of a particular user).

---
+++ help/app/helpers/auto_tickets_path_helper.rb
@@ -0,0 +1,51 @@
+#
+# These "auto" forms of the normal ticket path route helpers allow us to do two things automatically:
+#
+# (1) include the user in the path if appropriate.
+# (2) retain the sort params, if appropriate.
+#
+# Tickets views with a user_id are limited to that user. For admins, they don't need a user_id for any ticket action.
+#
+# This is available both to the views and the tickets_controller.
+#
+module AutoTicketsPathHelper
+
+  protected
+
+  def auto_tickets_path(options={})
+    options = ticket_view_options.merge options
+    if @user
+      user_tickets_path(@user, options)
+    else
+      tickets_path(options)
+    end
+  end
+
+  def auto_ticket_path(ticket, options={})
+    options = ticket_view_options.merge options
+    if @user
+      user_ticket_path(@user, ticket, options)
+    else
+      ticket_path(ticket, options)
+    end
+  end
+
+  def auto_new_ticket_path(options={})
+    options = ticket_view_options.merge options
+    if @user
+      new_user_ticket_path(@user, options)
+    else
+      new_ticket_path(options)
+    end
+  end
+
+  private
+
+  def ticket_view_options
+    hsh = {}
+    hsh[:open_status] = params[:open_status] if params[:open_status] && !params[:open_status].empty?
+    hsh[:sort_order]  = params[:sort_order]  if params[:sort_order]  && !params[:sort_order].empty?
+    hsh
+  end
+
+end
