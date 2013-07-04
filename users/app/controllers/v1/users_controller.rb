89ad6bd802f9e57c687e8cdb8593c3984e2fbd1b
elijah <elijah@riseup.net>
2013-07-04 01:33:09 -0700

    fix user typeahead

--- users/app/controllers/v1/users_controller.rb
+++ users/app/controllers/v1/users_controller.rb
@@ -2,10 +2,20 @@ module V1
   class UsersController < UsersBaseController
 
     skip_before_filter :verify_authenticity_token
-    before_filter :authorize, :only => [:update]
     before_filter :fetch_user, :only => [:update]
+    before_filter :authorize, :only => [:update]
+    before_filter :authorize_admin, :only => [:index]
 
     respond_to :json
+
+    def index
+      if params[:query]
+        @users = User.by_login.startkey(params[:query]).endkey(params[:query].succ)
+        respond_with @users.map(&:login).sort
+      else
+        render :json => {'error' => 'query required', 'status' => :unprocessable_entity}
+      end
+    end
 
     def create
       @user = User.create(params[:user])
