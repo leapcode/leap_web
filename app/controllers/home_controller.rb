1a3fdad01d9cb2e2596281d38ce0c0f1ad4da04a
elijah <elijah@riseup.net>
2013-07-31 15:12:59 -0700

    added empty 'home' layout and removed debugger from homecontroller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,9 @@
-
 class HomeController < ApplicationController
+  layout 'home'
+
   def index
     if logged_in?
       redirect_to user_overview_url(current_user)
     end
-    debugger if params[:debug]
   end
 end
