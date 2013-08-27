147ccec989672f9b1314aa6dcc5ce8578e841370
Azul <azul@leap.se>
2013-08-27 14:53:35 +0200

    do not redirect if no token present
    So far we allow two mechanisms of authentication:
      * session based
      * token based
    
    If token fails session will be atempted in most cases. So we can't just redirect here or we get a double render error.

--- users/app/controllers/controller_extension/token_authentication.rb
+++ users/app/controllers/controller_extension/token_authentication.rb
@@ -2,7 +2,7 @@ module ControllerExtension::TokenAuthentication
   extend ActiveSupport::Concern
 
   def token_authenticate
-    authenticate_or_request_with_http_token do |token_id, options|
+    authenticate_with_http_token do |token_id, options|
       @token = Token.find(token_id)
     end
     User.find_by_param(@token.user_id) if @token
