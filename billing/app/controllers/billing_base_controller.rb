77af7ac953fb22c181056e8c40c8749d12a63922
Azul <azul@leap.se>
2013-08-30 16:48:12 +0200

    also replace find_by_param in billing engine

--- billing/app/controllers/billing_base_controller.rb
+++ billing/app/controllers/billing_base_controller.rb
@@ -6,12 +6,12 @@ class BillingBaseController < ApplicationController
   # required for navigation to work.
   def assign_user
     if params[:user_id]
-      @user = User.find_by_param(params[:user_id])
+      @user = User.find(params[:user_id])
     elsif params[:action] == "confirm" or params[:action] == "destroy" # confirms and subscription deletes will come back with different ID set, so check for this first
       # This is only for cases where an admin cannot apply action for customer, but should be all confirms
       @user = current_user
     elsif params[:id]
-      @user = User.find_by_param(params[:id])
+      @user = User.find(params[:id])
     else
       # TODO
       # hacky, what are cases where @user hasn't yet been set? certainly some cases with subscriptions and payments
