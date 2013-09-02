1210f91251b14c251b2d4a6de7665fa520b77864
Azul <azul@leap.se>
2013-09-02 09:29:48 +0200

    Test the error handling of json and html responses

---
+++ test/functional/error_handling_test.rb
@@ -0,0 +1,20 @@
+require 'test_helper'
+
+class ErrorHandlingTest < ActionController::TestCase
+  tests HomeController
+
+  def setup
+    HomeController.any_instance.stubs(:index).raises
+  end
+
+  def test_json_error
+    get :index, format: :json
+    assert_equal 'application/json', @response.content_type
+  end
+
+  def test_html_error_reraises
+    assert_raises RuntimeError do
+      get :index
+    end
+  end
+end
