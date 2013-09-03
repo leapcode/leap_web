29b306a4d49d395e5753e2e85f8fa1f25d18410c
Azul <azul@leap.se>
2013-09-03 10:01:46 +0200

    remove unneccessary user creation and destruction in ticket test

--- help/app/models/ticket_comment.rb
+++ help/app/models/ticket_comment.rb
@@ -1,5 +1,5 @@
 class TicketComment
-  include CouchRest::Model::Embeddable 
+  include CouchRest::Model::Embeddable
 
   #belongs_to :ticket #is this best way to do it? will want to access all of a tickets comments, so maybe this isn't the way?
   property :posted_by, String#, :protected => true #Integer#this should be current_user if that is set, meaning the user is logged in  #cannot have it be protected and set via comments_attributes=. also, if it is protected and we set in the tickets_controller, it gets unset. TODO---is this okay to have it not protected and manually check it? We do not users to be able to set this.
@@ -23,17 +23,17 @@ class TicketComment
   end
 
   def posted_by_user
-    User.find(self.posted_by)
+    User.find(posted_by) if posted_by
   end
 
 =begin
-  #TODO. 
+  #TODO.
   #this is resetting all comments associated with the ticket:
   def set_time
     self.posted_at = Time.now
   end
 =end
-  
+
 =begin
   def set_posted_by
     self.posted_by = User.current if User.current
