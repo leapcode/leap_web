0fe1678cd37c8e917cb28eed9eb28777d3a92283
jessib <jessib@riseup.net>
2013-10-01 13:56:59 -0700

    Allow admins to view past-due subscriptions.

---
+++ billing/app/controllers/billing_admin_controller.rb
@@ -0,0 +1,14 @@
+class BillingAdminController < BillingBaseController
+  before_filter :authorize_admin
+
+  def show
+    @past_due_atleast_90_days = Braintree::Subscription.search do |search|
+      search.days_past_due >= 90
+    end
+
+    @all_past_due = Braintree::Subscription.search do |search|
+      search.status.is Braintree::Subscription::Status::PastDue
+    end
+  end
+
+end
--- billing/app/helpers/billing_helper.rb
+++ billing/app/helpers/billing_helper.rb
@@ -9,6 +9,15 @@ module BillingHelper
     form_for object, options, &block
   end
 
+  def billing_top_link(user)
+    # for admins, top link will show special admin information, which has link to show their own customer information
+    if (admin? and user == current_user)
+      billing_admin_path
+    else
+      show_or_new_customer_link(user)
+    end
+  end
+
   def show_or_new_customer_link(user)
     # Link to show if user is admin viewing another user, or user is already a customer.
     # Otherwise link to create a new customer.
@@ -17,6 +26,24 @@ module BillingHelper
     else
       new_customer_path
     end
+  end
+
+  # a bit strange to put here, but we don't have a subscription model
+  def user_for_subscription(subscription)
+
+    if (transaction = subscription.transactions.first)
+      # much quicker, but will only work if there is already a transaction associated with subscription (should generally be)
+      braintree_customer = transaction.customer_details
+    else
+      search_results = Braintree::Customer.search do |search|
+        search.payment_method_token.is subscription.payment_method_token
+      end
+      braintree_customer = search_results.first
+    end
+
+    customer = Customer.find_by_braintree_customer_id(braintree_customer.id)
+    user = User.find(customer.user_id)
+
   end
 
 end
