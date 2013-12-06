a013b03b0b715ec1209d2812da52ff5f0831c833
elijah <elijah@riseup.net>
2013-12-06 15:45:02 -0800

    make sure key responses are plain text

--- users/app/controllers/keys_controller.rb
+++ users/app/controllers/keys_controller.rb
@@ -1,12 +1,18 @@
 class KeysController < ApplicationController
 
+  #
+  # Render the user's key as plain text, without a layout.
+  #
+  # We will show blank page if user doesn't have key (which shouldn't generally occur)
+  # and a 404 error if user doesn't exist
+  #
   def show
     user = User.find_by_login(params[:login])
-    # layout won't be included if we render text
-    # we will show blank page if user doesn't have key (which shouldn't generally occur)
-    # and a 404 error if user doesn't exist
-    user ? (render text: user.public_key) : (raise ActionController::RoutingError.new('Not Found'))
-
+    if user
+      render text: user.public_key, content_type: 'text/text'
+    else
+      raise ActionController::RoutingError.new('Not Found')
+    end
   end
 
 end
