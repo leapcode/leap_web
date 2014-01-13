a19d01313ac534bdbbd8381593f887f1f77c94d5
jessib <jessib@riseup.net>
2014-01-13 11:28:14 -0800

    Comment for how to call view that we aren't now using.

--- users/app/designs/message/by_user_ids_to_show_and_created_at.js
+++ users/app/designs/message/by_user_ids_to_show_and_created_at.js
@@ -1,4 +1,5 @@
 // not using at moment
+// call with something like Message.by_user_ids_to_show_and_created_at.startkey([user_id, start_date]).endkey([user_id,end_date])
 function (doc) {
   if (doc.type === 'Message' && doc.user_ids_to_show && Array.isArray(doc.user_ids_to_show)) {
     doc.user_ids_to_show.forEach(function (userIdsToShow) {
