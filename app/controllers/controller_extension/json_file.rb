f07c952c870bfb8634ef0d80737b67a1eec760f6
Azul <azul@leap.se>
2014-07-14 13:04:30 +0200

    send config files from ConfigsController

---
+++ app/controllers/controller_extension/json_file.rb
@@ -0,0 +1,22 @@
+module ControllerExtension::JsonFile
+  extend ActiveSupport::Concern
+
+  protected
+
+  def send_file
+    if stale?(:last_modified => @file.mtime)
+      response.content_type = 'application/json'
+      render :text => @file.read
+    end
+  end
+
+  def fetch_file
+    if File.exists?(@filename)
+      @file = File.new(@filename)
+    else
+      not_found
+    end
+  end
+
+end
+
--- app/controllers/v1/configs_controller.rb
+++ app/controllers/v1/configs_controller.rb
@@ -1,20 +1,34 @@
 class V1::ConfigsController < ApiController
+  include ControllerExtension::JsonFile
 
   before_filter :require_login
+  before_filter :sanitize_filename, only: :show
+  before_filter :fetch_file, only: :show
 
   def index
-    render json: CONFIGS
+    render json: {services: service_paths}
   end
 
   def show
+    send_file
   end
 
-  CONFIGS = {
-    services: {
-      soledad: "/1/configs/soledad-service.json",
-      eip: "/1/configs/eip-service.json",
-      smtp: "/1/configs/smtp-service.json"
-    }
+  SERVICES = {
+    soledad: "soledad-service.json",
+    eip: "eip-service.json",
+    smtp: "smtp-service.json"
   }
 
+  protected
+
+  def service_paths
+    Hash[SERVICES.map{|k,v| [k,"/1/configs/#{str}"] } ]
+  end
+
+  def sanitize_filename
+    @filename = params[:id].downcase
+    @filename += '.json' unless @filename.ends_with?('.json')
+    access_denied unless SERVICES.values.include? name
+    @filename = Rails.root.join('public', '1', 'config', @filename)
+  end
 end
