b59360a003af595f1552f2ce753d4d80e5cf0028
elijah <elijah@riseup.net>
2014-12-10 15:05:34 -0800

    silently ignore 404 errors when trying to delete all user tickets: the tickets db might not yet be initialized.

--- engines/support/app/models/ticket.rb
+++ engines/support/app/models/ticket.rb
@@ -50,6 +50,8 @@ class Ticket < CouchRest::Model::Base
     self.by_created_by.key(user.id).each do |ticket|
       ticket.destroy
     end
+  rescue RestClient::ResourceNotFound
+    # silently ignore if design docs are not yet created
   end
 
   def is_creator_validated?
