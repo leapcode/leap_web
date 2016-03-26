7574862cdb8f43e9e4334924dc360f3d20b2c5fd
Azul <azul@riseup.net>
2016-03-26 18:33:30 +0100

    upgrade: horizontal forms now need column styles

--- config/initializers/simple_form_bootstrap.rb
+++ config/initializers/simple_form_bootstrap.rb
@@ -3,8 +3,8 @@ SimpleForm.setup do |config|
   config.wrappers :bootstrap, :tag => 'div', :class => 'form-group', :error_class => 'error' do |b|
     b.use :html5
     b.use :placeholder
-    b.use :label
-    b.wrapper :tag => 'div', :class => 'controls' do |ba|
+    b.use :label, :class => 'col-sm-2'
+    b.wrapper :tag => 'div', :class => 'col-sm-10' do |ba|
       ba.use :input
       ba.use :error, :wrap_with => { :tag => 'span', :class => 'help-inline' }
       ba.use :hint,  :wrap_with => { :tag => 'p', :class => 'help-block' }
