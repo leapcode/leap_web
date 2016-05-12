aeb0f39362b2195d4aec418349538b5f8443f693
Azul <azul@riseup.net>
2016-05-12 14:30:23 +0200

    minor: no need to vendor couchrest_session_store

--- vendor/gems/couchrest_session_store/test/setup_couch.sh
+++
@@ -1,7 +0,0 @@
-HOST="http://localhost:5984"
-echo "couch version :"
-curl -X GET $HOST
-
-curl -X PUT $HOST/couchrest_sessions
-curl -X PUT $HOST/couchrest_sessions/_design/Session --data @design/Session.json
-
