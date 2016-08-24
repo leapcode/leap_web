0df04c72719932eed9243ed132b60b1e01851752
luca-marie <ml.kochsiek@fu-berlin.de>
2016-08-24 15:23:07 +0200

    Update with Twitter

---
+++ app/controllers/twitter_controller.rb
@@ -0,0 +1,19 @@
+class TwitterController < ApplicationController
+  def twitter_client
+    Twitter::REST::Client.new do |config|
+      config.consumer_key = Rails.application.secrets.twitter['consumer_key']
+      config.consumer_secret = Rails.application.secrets.twitter['consumer_secret']
+      config.bearer_token = Rails.application.secrets.twitter['bearer_token']
+    end
+  end
+
+  def index
+    unless Rails.application.secrets.twitter['enabled'] == false
+      twitter_handle = Rails.application.secrets.twitter['twitter_handle']
+      @twitter_screen_name = twitter_handle
+      @twitter_name = twitter_client.user(twitter_handle).name
+      @tweets = twitter_client.user_timeline(twitter_handle).select{ |tweet| tweet.text.start_with?('RT','@')==false}
+      @tweet_time = "tweeted on"
+    end
+  end
+end
