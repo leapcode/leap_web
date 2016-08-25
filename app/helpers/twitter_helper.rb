ebb7c396f17f997d9e73cd2265942abb21d71db6
luca-marie <ml.kochsiek@fu-berlin.de>
2016-08-25 20:50:12 +0200

    Twitter controller now as helper, some more styling in CSS, bug fix

--- app/helpers/twitter_helper.rb
+++ app/helpers/twitter_helper.rb
@@ -1,2 +1,24 @@
 module TwitterHelper
+
+  def twitter_client
+    Twitter::REST::Client.new do |config|
+      config.consumer_key = Rails.application.secrets.twitter['consumer_key']
+      config.consumer_secret = Rails.application.secrets.twitter['consumer_secret']
+      config.bearer_token = Rails.application.secrets.twitter['bearer_token']
+    end
+  end
+
+  def twitter_handle
+    Rails.application.secrets.twitter['twitter_handle']
+  end
+
+  def twitter_name
+    twitter_client.user(twitter_handle).name
+  end
+
+  def tweets
+    twitter_client.user_timeline(twitter_handle).select{ |tweet| tweet.text.start_with?('RT','@')==false}
+  end
 end
+
+# unless Rails.application.secrets.twitter['enabled'] == false
