ea3fc6ffbb8f90aff28ce56fe4432a8d3ff18caa
luca-marie <ml.kochsiek@fu-berlin.de>
2016-08-29 17:39:46 +0200

    Changes mainly on CSS 'fine tuning' and shortening the Twitter API credentials, - only bearer token is needed to access Twitter API

--- app/helpers/twitter_helper.rb
+++ app/helpers/twitter_helper.rb
@@ -5,8 +5,6 @@ module TwitterHelper
 
   def twitter_client
     Twitter::REST::Client.new do |config|
-      config.consumer_key = Rails.application.secrets.twitter['consumer_key']
-      config.consumer_secret = Rails.application.secrets.twitter['consumer_secret']
       config.bearer_token = Rails.application.secrets.twitter['bearer_token']
     end
   end
