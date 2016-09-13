2ef8707a7539622007b6d679a7708a5979594ff3
luca-marie <ml.kochsiek@fu-berlin.de>
2016-09-13 12:20:12 +0200

    Cleaning CSS &view code, updating twitter helper with develop

--- app/helpers/twitter_helper.rb
+++ app/helpers/twitter_helper.rb
@@ -16,31 +16,42 @@ module TwitterHelper
   end
 
   def twitter_user_info
-    $twitter_user_info ||= []
+      $twitter_user_info ||= []
+  end
+
+  def twitter_name
+    if twitter_user_info[0] == nil
+      update_twitter_info
+    else
+      if Time.now > twitter_user_info[0] + 120.minutes
+        update_twitter_info
+      end
+    end
+    twitter_user_info[1]
   end
 
   def update_twitter_info
     twitter_user_info[0] = Time.now
     twitter_user_info[1] = twitter_client.user(twitter_handle).name
-    twitter_user_info[2] = twitter_client.user_timeline(twitter_handle).select{ |tweet| tweet.text.start_with?('RT','@')==false}.take(3)
   end
 
-  def cached_info
-    if twitter_user_info[0] == nil
-      update_twitter_info
+  def twitter_tweets
+      $twitter_tweets ||= []
+  end
+
+  def twitter_timeline
+    if twitter_tweets[0] == nil
+      update_twitter_timeline
     else
-      if Time.now > twitter_user_info[0] + 15.minutes
-        update_twitter_info
+      if Time.now > twitter_tweets[0] + 15.minutes
+        update_twitter_timeline
       end
     end
-    twitter_user_info
+    twitter_tweets[1]
   end
 
-  def twitter_name
-    cached_info[1]
-  end
-
-  def tweets
-    cached_info[2]
+  def update_twitter_timeline
+    twitter_tweets[0] = Time.now
+    twitter_tweets[1] = twitter_client.user_timeline(twitter_handle).select{ |tweet| tweet.text.start_with?('RT','@')==false}.take(3)
   end
 end
