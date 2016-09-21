460129e5965751127bb6cb363a661afb7c7d5ab4
thea <ta.kupler@gmail.com>
2016-09-21 14:08:38 +0200

    included error-handling: twitter-helper includes rescue and error message for fourth value in array. there will always be an empty array that view (text and each - method) does not break. if empty array in view the error-message is shown. (its WIP)

--- app/helpers/twitter_helper.rb
+++ app/helpers/twitter_helper.rb
@@ -23,6 +23,26 @@ module TwitterHelper
     twitter_user_info[0] = Time.now
     twitter_user_info[1] = twitter_client.user(twitter_handle).name
     twitter_user_info[2] = twitter_client.user_timeline(twitter_handle).select{ |tweet| tweet.text.start_with?('RT','@')==false}.take(3)
+    if twitter_user_info[2] == nil
+      error_handling
+      twitter_user_info[3] = "The twitter-handle does not exist or the account is private. Please change it or contact your provider-admin."
+    end
+  rescue Twitter::Error::BadRequest
+    error_handling
+  rescue Twitter::Error::Unauthorized
+    error_handling
+  rescue Twitter::Error::Forbidden
+    error_handling
+  rescue Twitter::Error::NotAcceptable
+    error_handling
+  rescue Twitter::Error::TooManyRequests
+    error_handling
+  end
+
+  def error_handling
+    twitter_user_info[2] = []
+    twitter_user_info[3] = "The twitter-handle does not exist or the account is private. Please change it or contact your provider-admin."
+    return twitter_user_info
   end
 
   def cached_info
@@ -42,5 +62,9 @@ module TwitterHelper
 
   def tweets
     cached_info[2]
+  end
+
+  def error_message
+    cached_info[3]
   end
 end
