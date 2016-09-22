306b36c045cd62638db179115ed002c6ab7c5cd8
luca-marie <ml.kochsiek@fu-berlin.de>
2016-09-22 14:36:10 +0200

    included Theas work on error-handling \o/; changed link to twitter-account in header; fixed erb escaping characters problem without letting evil code pass; setting customized image file 'Avatar_Pic.png' in config/custo../assets to get loaded instead of default twitter-logo

--- app/helpers/twitter_helper.rb
+++ app/helpers/twitter_helper.rb
@@ -23,6 +23,30 @@ module TwitterHelper
     twitter_user_info[0] = Time.now
     twitter_user_info[1] = twitter_client.user(twitter_handle).name
     twitter_user_info[2] = twitter_client.user_timeline(twitter_handle).select{ |tweet| tweet.text.start_with?('RT','@')==false}.take(3)
+    if twitter_user_info[2] == nil
+      error_handling
+      twitter_user_info[3] = "The twitter-handle does not exist or the account is private. Please change it or contact your provider-admin."
+    end
+  rescue Twitter::Error::BadRequest
+    error_handling
+    twitter_user_info[3] = "The request to have the tweets shown was invalid or cannot be otherwise served."
+  rescue Twitter::Error::Unauthorized
+    error_handling
+    twitter_user_info[3] = "Please change your twitter-credentials, make sure that the twitter-account you access is public or contact your provider-admin to have the tweets shown."
+  rescue Twitter::Error::Forbidden
+    error_handling
+    twitter_user_info[3] = "The request to have the tweets shown is understood, but it has been refused or access is not allowed."
+  rescue Twitter::Error::NotAcceptable
+    error_handling
+    twitter_user_info[3] = "An invalid format is specified in the request to have the tweets shown."
+  rescue Twitter::Error::TooManyRequests
+    error_handling
+    twitter_user_info[3] = "The rate-limit for accessing the tweets is reached. You should be able to see the tweets in a couple of minutes."
+  end
+
+  def error_handling
+    twitter_user_info[2] = []
+    return twitter_user_info
   end
 
   def cached_info
@@ -42,5 +66,9 @@ module TwitterHelper
 
   def tweets
     cached_info[2]
+  end
+
+  def error_message
+    cached_info[3]
   end
 end
