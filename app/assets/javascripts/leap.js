49618effe081ebf5e72a5945fa1822c471369cf0
Azul <azul@riseup.net>
2017-04-03 09:21:20 +0200

    fix: error message display in production
    only use the <pre> tag if the response was a text message i.e. during dev errors.
    Use the alert-danger class of bootstrap 3 rather than alert-error.

--- app/assets/javascripts/leap.js
+++ app/assets/javascripts/leap.js
@@ -3,7 +3,7 @@
 // add a bootstrap alert to the page via javascript.
 //
 function alert_message(msg) {
-  $('#messages').append('<div class="alert alert-error"><a class="close" data-dismiss="alert">×</a><pre>'+msg+'</pre></div>');
+  $('#messages').append('<div class="alert alert-danger"><a class="close" data-dismiss="alert">×</a>'+msg+'</div>');
 }
 
 ClientSideValidations.formBuilders['SimpleForm::FormBuilder'].wrappers.bootstrap = ClientSideValidations.formBuilders['SimpleForm::FormBuilder'].wrappers["default"];
